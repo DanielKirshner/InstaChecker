311e41bb6413d6d328c36ef4f55d435cc7e8f582
DanielKirshner <danielkirshner7878@gmail.com>
2024-12-26 23:51:27 +0200

    Added error and edge cases handling; improved output

--- instachecker.py
+++ instachecker.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from rich import print
 from typing import Set
 from enum import Enum
 
@@ -10,24 +11,36 @@ class FilePath(Enum):
 
 
 def extract_usernames(file_path: str) -> Set[str]:
-    with open(file_path, 'r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
+    try:
         usernames = set()
-        
-        for link in soup.find_all('a', href=True):
-            if "instagram.com" in link['href']:
-                usernames.add(link.text.strip())                
-        return usernames
+        with open(file_path, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'html.parser')
 
+            for link in soup.find_all('a', href=True):
+                if "instagram.com" in link['href']:
+                    usernames.add(link.text.strip())
+    except FileNotFoundError:
+        print(f"[bold red]Error: File '{file_path}' not found.")
+    except Exception as e:
+        print(f"[bold red]An error occurred while reading '{file_path}': {e}")
+    
+    return usernames
 
 def main():
     followers = extract_usernames(FilePath.FOLLOWERS.value)
     following = extract_usernames(FilePath.FOLLOWING.value)
 
+    if not followers or not following:
+        return
+
     not_following_back = following - followers
 
-    print("Accounts you follow but who don't follow you back:\n")
-    print(sorted(not_following_back))
+    print("\nAccounts you follow but who don't follow you back:")
+    if not_following_back:
+        for username in sorted(not_following_back):
+            print(f"[bold yellow]{username}")
+    else:
+        print("[bold green]None! Everyone you follow follows you back.\n")
 
 
 if __name__ == "__main__":
